25ee3eecd44f60029fed51a4416e5a0998cd19cc
syoung <syoung1394@naver.com>
2017-10-18 20:24:01 +0900

    modify js

--- battleship.js
+++ battleship.js
@@ -1,33 +1,35 @@
-var location1=3;
-var location2=4;
-var location3=5;
+var randomLoc;
+var location1 = randomLoc;
+var location2 = location1+1;
+var location3 = location2+1;
 
 var guess;
-var hits=0;
-var guesses=0;
-var isSunk=false;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
 
-while(isSunk==false){
-  guess=prompt("준비, 조준, 발사!(0에서 6까지 중 추측한 숫자를 입력하세요):");
-  if (guess<0 || guess>6){
+while (isSunk == false) {
+  guess = prompt("준비, 조준, 발사!(0에서 6까지 중 추측한 숫자를 입력하세요):");
+  if (guess < 0 || guess > 6) {
     alert("올바른 방 번호를 입력하세요");
-  }else{
-  guesses=guesses+1;
+  } else {
+    guesses = guesses + 1;
+
+    if (guess == location1 || guess == location2 || guess == location3) {
+      hits = hits + 1;
+      alert("명중!");
+      if (hits == 3) {
+        alert("전함 침몰");
+        isSunk = true;
+      }
+    } else {
+      alert("실패!");
+    }
   }
 }
 
-
-if(guess==location1 ||guess==location2 || guess==location3){
-hits=hits+1;
-}
-
-if(hits==3){
- alert("전함 침몰");
-isSunk=true;
-}
-
 //게임 종료 결과 알려주기
-var stats="여러분은 전함을 격침시키기 위해"+guesses+"번 발사했습니다"+
-          "따라서 명중률은"+(3/guesses)+"입니다";
+var stats = "여러분은 전함을 격침시키기 위해" + guesses + "번 발사했습니다" +
+  "따라서 명중률은" + (3 / guesses) + "입니다";
 
 alert(stats);
